e8f4defdea0f7c7cc063066267ca671117864e21
asif1416 <ashrafulasif260@gmail.com>
2025-02-09 19:25:16 +0600

    connecting

--- app/menu/[id]/page.tsx
+++ app/menu/[id]/page.tsx
@@ -4,28 +4,24 @@ import MenuItemDetails from "../MenuItemDetails";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-export default async function MenuItemPage({params,}: {
-    params: { id: string };
+export default async function MenuItemPage({
+  params,
+}: {
+  params: { id: string };
 }) {
-    const { id } =  params;
+  const menuItem = await fetchMenuItem(params.id);
 
-    if (!id) {
-        notFound();
-    }
+  if (!menuItem) {
+    notFound();
+  }
 
-    const menuItem = await fetchMenuItem(id);
-
-    if (!menuItem) {
-        notFound();
-    }
-
-    return (
-        <>
-            <Navbar />
-            <div className="mx-auto w-2/3">
-                <MenuItemDetails menuItem={menuItem} />
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="mx-auto w-2/3">
+        <MenuItemDetails menuItem={menuItem} />
+      </div>
+      <Footer />
+    </>
+  );
 }
