6ed59400fe56a25aaa1f1b25c5c20d05b7a71a06
asif1416 <ashrafulasif260@gmail.com>
2025-02-09 19:41:14 +0600

    connecting

--- app/menu/[id]/page.tsx
+++ app/menu/[id]/page.tsx
@@ -3,7 +3,12 @@ import { fetchMenuItem } from "@/api/menu";
 import MenuItemDetails from "../MenuItemDetails";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { PageProps } from "@/types/types";
+
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
 export default async function MenuItemPage({ params }: PageProps) {
   const menuItem = await fetchMenuItem(params.id);
@@ -22,4 +27,3 @@ export default async function MenuItemPage({ params }: PageProps) {
     </>
   );
 }
-
