1382a185ce4a8d86cf8ef8f880829d05a70e3d58
Asif259 <ashrafulasif259@gmail.com>
2025-02-11 11:38:57 +0600

    connecting

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://lively-stillness-production.up.railway.app/",
+  baseURL: "https://backend-phi-one-40.vercel.app/",
   withCredentials: true,
 });
 
